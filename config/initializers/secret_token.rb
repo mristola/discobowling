f85aec3b79a70a89fab655469d8fbee6b18e2dbd
Markus Ristola <markus@ristola.se>
2013-02-01 15:58:35 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Discobowling::Application.config.secret_token = '87f353193c072d19ecb3915e81376a31079e424c42c2088006037514e5f26f427416c382cfd469ab911c8ce23a1d6c9a77ddcfca2a453a024a3a2143b0d7228d'
